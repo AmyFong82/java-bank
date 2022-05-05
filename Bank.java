068dd83a1db69cebdbefe6b1ad554a5b3d7711f9
AmyFong82 <amyhfong@gmail.com>
2022-05-05 10:49:28 -0400

    Working version to print Bank's interest. Will do the PrintAllInterests next.

--- Bank.java
+++ Bank.java
@@ -1,21 +1,30 @@
 class Bank {
-    float rateOfInterest = 0.1;
-    public void getRateOfInterest(rateOfInterest){
-      return rateOfInterest;
-    }
+  static double getRateOfInterest(){return 0.1;}
+  // public void getRateOfInterest(){
+  //   System.out.println(rateOfInterest);
+  // }
+  public static void main(String[] args){
+    System.out.println(getRateOfInterest());
   }
 }
 
 class SBI extends Bank {
-  float rateOfInterest = 0.08;
+  static double getRateOfInterest(){return 0.08;}
+}
+
+// class ICICI extends Bank {
+//   double rateOfInterest = 0.07;
+// }
+
+// class AXIS extends Bank {
+//   double rateOfInterest = 0.09;
+// }
+
+class PrintAllInterests{
+  public static void main(String[] args){
+    Bank b = new Bank();
+    b.getRateOfInterest();
+    SBI a = new SBI();
+    a.getRateOfInterest();
   }
-}
-
-class ICICI extends Bank {
-  float rateOfInterest = 0.07;
-
-}
-
-class AXIS extends Bank {
-  float rateOfInterest = 0.09;
 }
