73a15c3933073810876a9ea534afea293fc1e864
AmyFong82 <amyhfong@gmail.com>
2022-05-05 09:29:23 -0400

    added 3 children class that entends Bank class.

--- Bank.java
+++ Bank.java
@@ -5,5 +5,19 @@ class Bank {
 }
 
 class SBI extends Bank {
+  public void float getRateOfInterest() {
+    return 0.08;
+  }
+}
 
+class ICICI extends Bank {
+  public void float getRateOfInterest() {
+    return 0.07;
+  }
+}
+
+class AXIS extends Bank {
+  public void float getRateOfInterest() {
+    return 0.09;
+  }
 }
